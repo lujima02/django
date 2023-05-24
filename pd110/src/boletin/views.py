1cc3eaa9795d64edfe83585599a8391cdbe5503e
lujima02 <lujima2002@gmail.com>
2023-05-24 05:40:58 +0200

    15commit

--- pd110/src/boletin/views.py
+++ pd110/src/boletin/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
 
 from .forms import RegModelForm, ContactForm
 from .models import Registrado
@@ -43,9 +45,19 @@ def inicio(request):
 def contact(request):
     form = ContactForm(request.POST or None)
     if form.is_valid():
-        email = form.cleaned_data.get("email")
-        mensaje = form.cleaned_data.get("mensaje")
-        nombre = form.cleaned_data.get("nombre")
+        form_email = form.cleaned_data.get("email")
+        form_mensaje = form.cleaned_data.get("mensaje")
+        form_nombre = form.cleaned_data.get("nombre")
+        asunto = 'Form de Contato'
+        email_from = settings.EMAIL_HOST_USER
+        email_to = [email_from, "otroemail@gmail.com"]
+        email_mensaje = "%s: %s enviado por %s" %(form_nombre, form_mensaje, form_email)
+        send_mail(asunto,
+                  email_mensaje,
+                  email_from,
+                  email_to,
+                  fail_silently=False
+                  )
     context = {
         "form": form,
     }
