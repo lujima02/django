4a7180f00f3616ddf807bff466513d95b54f8065
lujima02 <lujima2002@gmail.com>
2023-05-24 06:17:12 +0200

    16commit

--- pd110/src/pd110/settings.py
+++ pd110/src/pd110/settings.py
@@ -123,6 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static_pro' / 'static',
+]
+
+STATIC_ROOT = BASE_DIR.parent / 'static_env' / 'static_root'
+MEDIA_ROOT = BASE_DIR.parent / 'static_env' / 'media_root'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- pd110/src/pd110/urls.py
+++ pd110/src/pd110/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from boletin import views
 
@@ -24,3 +26,7 @@ urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('contact/', views.contact, name='contact'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
