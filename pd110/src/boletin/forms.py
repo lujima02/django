12dbcdc0de0a7df5019ff24206124e0c94756cc9
lujima02 <lujima2002@gmail.com>
2023-05-24 04:05:17 +0200

    11commit

--- pd110/src/boletin/forms.py
+++ pd110/src/boletin/forms.py
@@ -6,6 +6,18 @@ class RegModelForm(forms.ModelForm):
     class Meta:
         model = Registrado
         fields = ["nombre", "email"]
+    
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        email_base, proveerdor = email.split("@")
+        dominio, extension = proveerdor.split(".")
+        if not extension == "edu":
+            raise forms.ValidationError("Por favor utiliza un email con la extension .edu")
+        return email
+    
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get("nombre")
+        return nombre
 
 class RegForm(forms.Form):
     nombre = forms.CharField(max_length=100)
