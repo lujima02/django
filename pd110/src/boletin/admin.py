1d7b4ed25309591b76826acc554bb36d051de420
lujima02 <lujima2002@gmail.com>
2023-05-24 03:48:57 +0200

    10commit

--- pd110/src/boletin/admin.py
+++ pd110/src/boletin/admin.py
@@ -2,15 +2,17 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Registrado
+from .forms import RegModelForm
 
 class AdminRegistrado(admin.ModelAdmin):
     list_display = ["email", "nombre", "timestamp"]
+    form = RegModelForm
     #list_display_links = ["nombre"]
     list_filter = ["timestamp"]
     list_editable = ["nombre"]
     search_fields = ["email", "nombre"]
-    class Meta:
-        model = Registrado
+    #class Meta:
+    #    model = Registrado
 
 
 admin.site.register(Registrado, AdminRegistrado)
